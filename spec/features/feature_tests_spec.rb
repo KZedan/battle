1b6b7528c841089e9e8fc2ed249701bf7ebaea2b
Rianne McCartney <riannemccartney@hotmail.com>
2019-07-09 16:53:14 +0100

    Implementing hit points with lots of mistakes

--- spec/features/feature_tests_spec.rb
+++ spec/features/feature_tests_spec.rb
@@ -19,3 +19,13 @@ end
       expect(page).to have_content "Dave attacked Mittens"
     end
   end
+
+feature 'deducts points' do
+  scenario 'reduces players HP' do
+    sign_in_and_play
+    click_button 'Attack'
+    click_link 'OK'
+    expect(page).to have_content "Mittens: 50HP"
+    expect(page).not_to have_content "Mittens: 60HP"
+  end
+end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,9 +1,23 @@
 describe Player do
   subject(:dave) {Player.new('Dave')}
+  subject(:mittens) {Player.new('Mittens')}
 
   describe '#name' do
     it "returns it's name" do
-      expect(subject.name).to eq 'Dave'
+      expect(dave.name).to eq 'Dave'
     end
-  end 
+
+  it 'returns the hit points' do
+      expect(dave.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+    end
+
+  it 'damages the player' do
+    expect(mittens).to receive(:receive_damage)
+    dave.attack(mittens)
+  end
+
+  it 'reduces the player hit points' do
+    expect { dave.receive_damage }.to change { dave.hit_points }.by(-10)
+  end
 end
+end 
